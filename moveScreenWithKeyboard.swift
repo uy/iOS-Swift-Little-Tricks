3b1e0e551527414d0ad03defbab754fa8d614c4e
Utku Y <utkuyegen@gmail.com>
2017-08-15 23:11:04 +0300

    Create moveScreenWithKeyboard.swift

---
+++ moveScreenWithKeyboard.swift
@@ -0,0 +1,21 @@
+override func viewDidLoad() {
+    super.viewDidLoad()            
+    NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardWillShow), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+    NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardWillHide), name: NSNotification.Name.UIKeyboardWillHide, object: nil)    
+}
+
+func keyboardWillShow(notification: NSNotification) {        
+    if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+        if self.view.frame.origin.y == 0{
+            self.view.frame.origin.y -= keyboardSize.height
+        }
+    }        
+}
+
+func keyboardWillHide(notification: NSNotification) {
+    if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+        if self.view.frame.origin.y != 0{
+            self.view.frame.origin.y += keyboardSize.height
+        }
+    }
+}
