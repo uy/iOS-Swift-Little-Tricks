898b5ece90438668c41adbd70cea46ce56c04883
Utku Y <utkuyegen@gmail.com>
2017-08-19 20:30:30 +0300

    Create SimpleToast.swift

---
+++ SimpleToast.swift
@@ -0,0 +1,43 @@
+
+extension UIViewController {
+    func showToast(message : String) {
+        let toastLabel = UILabel(frame: CGRect(x: self.view.frame.size.width/2 - 125, y: self.view.frame.size.height-100, width: 250, height: 35))
+        toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+        toastLabel.textColor = UIColor.white
+        toastLabel.textAlignment = .center;
+        toastLabel.font = UIFont.systemFont(ofSize: UIFont.systemFontSize) //UIFont(name: "Montserrat-Light", size: 12.0)
+        toastLabel.adjustsFontSizeToFitWidth = true
+        toastLabel.text = message
+        toastLabel.alpha = 2.5
+        toastLabel.layer.cornerRadius = 10;
+        toastLabel.clipsToBounds = true
+        
+        self.view.addSubview(toastLabel)
+        UIView.animate(withDuration: 1.5, delay: 0.1, options: .curveEaseOut, animations: {
+            toastLabel.alpha = 0.0
+        }, completion: {(isCompleted) in
+            toastLabel.removeFromSuperview()
+        })
+    }
+    
+    func showToast(message: String, comp: @escaping () -> Void ) {
+        let toastLabel = UILabel(frame: CGRect(x: self.view.frame.size.width/2 - 125, y: self.view.frame.size.height-100, width: 250, height: 35))
+        toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+        toastLabel.textColor = UIColor.white
+        toastLabel.textAlignment = .center;
+        toastLabel.font = UIFont.systemFont(ofSize: UIFont.systemFontSize) //UIFont(name: "Montserrat-Light", size: 12.0)
+        toastLabel.adjustsFontSizeToFitWidth = true
+        toastLabel.text = message
+        toastLabel.alpha = 2.5
+        toastLabel.layer.cornerRadius = 10;
+        toastLabel.clipsToBounds = true
+        
+        self.view.addSubview(toastLabel)
+        UIView.animate(withDuration: 1.5, delay: 0.1, options: .curveEaseOut, animations: {
+            toastLabel.alpha = 0.0
+        }, completion: {(isCompleted) in
+            toastLabel.removeFromSuperview()
+            comp()
+        })
+    }
+}
