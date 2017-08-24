a28a295031e84c49123058dbadf2be705e6d67b1
Utku Y <utkuyegen@gmail.com>
2017-08-24 14:21:08 +0300

    Create SimpleCheckbox.swift

---
+++ SimpleCheckbox.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+class CheckBox: UIButton {
+    // Images
+    let checkedImage = UIImage(named: "ic_check_box")! as UIImage
+    let uncheckedImage = UIImage(named: "ic_check_box_outline_blank")! as UIImage
+
+    // Bool property
+    var isChecked: Bool = false {
+        didSet{
+            if isChecked == true {
+                self.setImage(checkedImage, forState: .Normal)
+            } else {
+                self.setImage(uncheckedImage, forState: .Normal)
+            }
+        }
+    }
+
+    override func awakeFromNib() {
+        self.addTarget(self, action: "buttonClicked:", forControlEvents: UIControlEvents.TouchUpInside)
+        self.isChecked = false
+    }
+
+    func buttonClicked(sender: UIButton) {
+        if sender == self {
+            isChecked = !isChecked
+        }
+    }
+}
