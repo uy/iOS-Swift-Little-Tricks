974f1e681ac6568f34b4314c8e2ae03e361d4c79
Utku Y <utkuyegen@gmail.com>
2017-08-24 14:22:15 +0300

    Create CustomCheckbox.swift

---
+++ Custom Components/CustomCheckbox.swift
@@ -0,0 +1,30 @@
+// based on http://stackoverflow.com/questions/29117759/how-to-create-radio-buttons-and-checkbox-in-swift-ios
+import UIKit
+
+class CheckBox: UIButton {
+    // Images
+    let checkedImage = UIImage(named: "ic_check_box")! as UIImage
+    let uncheckedImage = UIImage(named: "ic_check_box_outline_blank")! as UIImage
+
+    // Bool property
+    var isChecked: Bool = false {
+        didSet{
+            if isChecked == true {
+                self.setImage(checkedImage, forState: .Normal)
+            } else {
+                self.setImage(uncheckedImage, forState: .Normal)
+            }
+        }
+    }
+
+    override func awakeFromNib() {
+        self.addTarget(self, action: "buttonClicked:", forControlEvents: UIControlEvents.TouchUpInside)
+        self.isChecked = false
+    }
+
+    func buttonClicked(sender: UIButton) {
+        if sender == self {
+            isChecked = !isChecked
+        }
+    }
+}
