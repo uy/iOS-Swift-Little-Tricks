29c4dd5440811572ba9061aa108cee619eecd15e
Utku Y <utkuyegen@gmail.com>
2017-10-24 22:58:17 +0300

    Update moveScreenWithKeyboard.swift

--- moveScreenWithKeyboard.swift
+++ moveScreenWithKeyboard.swift
@@ -6,6 +6,12 @@ override func viewDidLoad() {
     NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardWillHide), name: NSNotification.Name.UIKeyboardWillHide, object: nil)    
 }
 
+override func viewWillDisappear(animated: Bool) {
+    super.viewWillDisappear(animated)
+    NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillShowNotification, object: self.view.window)
+    NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillHideNotification, object: self.view.window)
+}
+
 func keyboardWillShow(notification: NSNotification) {        
     if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
         if self.view.frame.origin.y == 0{
