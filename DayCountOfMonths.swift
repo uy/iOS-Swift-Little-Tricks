3f071243d75de9dfe30a928276a342d6ec9d2235
Utku Y <utkuyegen@gmail.com>
2018-01-31 20:43:06 +0300

    Update DayCountOfMonths.swift

--- DayCountOfMonths.swift
+++ DayCountOfMonths.swift
@@ -9,9 +9,23 @@ let components: Set<Calendar.Component> = [
 let dateComponents = calendar.dateComponents(components, from: date)
 
 for idx in 1...12 {
-    let swapDateComponents = DateComponents(year: dateComponents.year, month: idx, hour: 15)
+    let swapDateComponents = DateComponents(year: dateComponents.year, month: idx)
     let swapDate = calendar.date(from: swapDateComponents)!
     let range = calendar.range(of: .day, in: .month, for: swapDate)
     let numberOfDays = range?.count
     print(numberOfDays)
 }
+
+// Result Output
+// Optional(31)
+// Optional(28)
+// Optional(31)
+// Optional(30)
+// Optional(31)
+// Optional(30)
+// Optional(31)
+// Optional(31)
+// Optional(30)
+// Optional(31)
+// Optional(30)
+// Optional(31)
