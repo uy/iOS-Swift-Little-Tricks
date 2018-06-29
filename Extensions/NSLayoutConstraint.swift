510b0d82830a466619024904b7d35c690cc43c87
Utku Y <utkuyegen@gmail.com>
2018-06-29 16:41:54 +0300

    Update NSLayoutConstraint.swift

--- Extensions/NSLayoutConstraint.swift
+++ Extensions/NSLayoutConstraint.swift
@@ -3,24 +3,62 @@ enum ConstraintType {
     case rightToRight
     case leftToLeft
     case toTop
+    case toBottom
+    case bottomToTop
+    case topToBottom
+    case rightToLeft
     case leftToRight
-    case centerToCenter
+    case centerToCenterY
+    case centerToCenterX
+    case height
+    case width
+    case ratio
 }
 
 // Simple way to make constraint
 extension NSLayoutConstraint {
-    convenience init(this: Any, matchLike: ConstraintType, to: Any?, by: CGFloat) {
+    convenience init(this: Any, matchLike: ConstraintType, to: Any?, by: CGFloat = 0, multiplier: CGFloat = 1) {
         switch matchLike {
+            
         case .rightToRight:
-            self.init(item: this, attribute: .trailing, relatedBy: .equal, toItem: to, attribute: .trailing, multiplier: Constants.constraintMultiplier1, constant: by * -1)
+            self.init(item: this, attribute: .trailing, relatedBy: .equal, toItem: to, attribute: .trailing, multiplier: multiplier, constant: by * -1)
+            
         case .leftToLeft:
-            self.init(item: this, attribute: .leading, relatedBy: .equal, toItem: to, attribute: .leading, multiplier: Constants.constraintMultiplier1, constant: by)
+            self.init(item: this, attribute: .leading, relatedBy: .equal, toItem: to, attribute: .leading, multiplier: multiplier, constant: by)
+            
         case .toTop:
-            self.init(item: this, attribute: .top, relatedBy: .equal, toItem: to, attribute: .top, multiplier: Constants.constraintMultiplier1, constant: by)
+            self.init(item: this, attribute: .top, relatedBy: .equal, toItem: to, attribute: .top, multiplier: multiplier, constant: by)
+            
+        case .toBottom:
+            self.init(item: this, attribute: .bottom, relatedBy: .equal, toItem: to, attribute: .bottom, multiplier: multiplier, constant: by * -1)
+            
+        case .topToBottom:
+            self.init(item: this, attribute: .top, relatedBy: .equal, toItem: to, attribute: .bottom, multiplier: multiplier, constant: by)
+            
+        case .bottomToTop:
+            self.init(item: this, attribute: .bottom, relatedBy: .equal, toItem: to, attribute: .top, multiplier: multiplier, constant: by)
+            
+        case .rightToLeft:
+            self.init(item: this, attribute: .trailing, relatedBy: .equal, toItem: to, attribute: .leading, multiplier: multiplier, constant: by)
+            
         case .leftToRight:
-            self.init(item: this, attribute: .leading, relatedBy: .equal, toItem: to, attribute: .trailing, multiplier: Constants.constraintMultiplier1, constant: by)
-        case .centerToCenter:
-            self.init(item: this, attribute: .centerY, relatedBy: .equal, toItem: to, attribute: .centerY, multiplier: Constants.constraintMultiplier1, constant: by)
+            self.init(item: this, attribute: .leading, relatedBy: .equal, toItem: to, attribute: .trailing, multiplier: multiplier, constant: by)
+            
+        case .centerToCenterY:
+            self.init(item: this, attribute: .centerY, relatedBy: .equal, toItem: to, attribute: .centerY, multiplier: multiplier, constant: by)
+            
+        case .centerToCenterX:
+            self.init(item: this, attribute: .centerX, relatedBy: .equal, toItem: to, attribute: .centerX, multiplier: multiplier, constant: by)
+            
+        case .height:
+            self.init(item: this, attribute: .height, relatedBy: .equal, toItem: to, attribute: .height, multiplier: multiplier, constant: by)
+            
+        case .width:
+            self.init(item: this, attribute: .width, relatedBy: .equal, toItem: to, attribute: .width, multiplier: multiplier, constant: by)
+            
+        case .ratio:
+            self.init(item: this, attribute: .height, relatedBy: .equal, toItem: to, attribute: .width, multiplier: multiplier, constant: by)
+            
         }
     }
 }
